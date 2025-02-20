a30ee652c3129e9c01438fe0644c2a067019b68b
Kirill Kireev <kakhmetgareev@mail.ru>
2025-02-20 09:02:22 +0300

    1.1

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,3 +1,4 @@
+import { sequelize } from '../config/db.js';
 import { Users } from '../models/users.model.js';
 
 async function depositBalance(userId, amount) {
@@ -12,30 +13,40 @@ async function depositBalance(userId, amount) {
 }
 
 async function withdrawBalance(userId, amount) {
-    const user = await Users.findByPk(userId);
+    const t = await sequelize.transaction();
+    try {
+        const user = await Users.findByPk(userId, {
+            transaction: t,
+            lock: t.LOCK.UPDATE,
+        });
 
-    if (!user) {
-        const error = new Error();
-        error.data = {
-            error: 'error user',
-            message: `Пользователь с id: ${userId} не найден`,
-        };
+        if (!user) {
+            const error = new Error();
+            error.data = {
+                error: 'error user',
+                message: `Пользователь с id: ${userId} не найден`,
+            };
+            throw error;
+        }
+
+        if (user.balance < amount) {
+            const error = new Error();
+            error.data = {
+                error: 'error balance',
+                message: `Пользователь не может иметь отрицательный баланс`,
+            };
+            throw error;
+        }
+
+        user.balance -= amount;
+        await user.save({ transaction: t });
+        await t.commit();
+        return user.balance;
+    } catch (error) {
+        await t.rollback();
+        console.error('Transaction failed:', error);
         throw error;
     }
-
-    if (user.balance < amount) {
-        const error = new Error();
-        error.data = {
-            error: 'error balance',
-            message: `Пользователь не может иметь отрицательный баланс`,
-        };
-        throw error;
-    }
-
-    user.balance -= amount;
-
-    await user.save();
-    return user.balance;
 }
 
 export { depositBalance, withdrawBalance };
